c53a0b6ab0ad260e840b83ec22f42a9a9f75cef7
Ryan Balfanz <ryan@ryanbalfanz.net>
2011-12-19 13:52:56 -0800

    Added initial README.md and setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+htmlpretty
+----------
+
+Prettify your HTML.
+"""
+from setuptools import setup
+
+
+setup(
+    name='htmlpretty',
+    version='0.1.0',
+    url='https://github.com/RyanBalfanz/htmlpretty',
+    license='MIT',
+    author='Ryan Balfanz',
+    author_email='ryan@ryanbalfanz.net',
+    description='Prettify your HTML.',
+    long_description=__doc__,
+    packages=['htmlpretty.py'],
+    # namespace_packages=['htmlpretty'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'BeautifulSoup',
+    ],
+    classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 2.7",
+		"Topic :: Internet",
+		"Topic :: Text Processing",
+		"Topic :: Text Processing :: Markup :: HTML",
+		"Topic :: Utilities",
+    ]
+)
